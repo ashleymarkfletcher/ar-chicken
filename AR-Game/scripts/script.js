2d335c9241c2a7f69c9879f7d947bee6e5df40a5
ashley mark fletcher <ashleymarkfletcher@gmail.com>
2018-04-24 19:13:44 +0100

    Fix eating and lose life

--- AR-Game/scripts/script.js
+++ AR-Game/scripts/script.js
@@ -8,14 +8,17 @@ var Reactive = require('Reactive')
 
 // main scene objects
 var sceneRoot = Scene.root.child("Device").child("Camera").child("Focal Distance")
-var scoreText = sceneRoot.child("2DCanvas0").child("text0")
+var scoreTextContainer = sceneRoot.child("2DCanvas0")
+var scoreText = scoreTextContainer.child("scoreText")
 var livesText = sceneRoot.child("lives").child("2DCanvas1").child("text1")
 var faceTracker = Scene.root.child("Device").child("Camera").child("Focal Distance").child("facetracker0")
 var mouthIsOpen = FaceTracking.face(0).mouth.openness.gt(0.3).and(FaceTracking.count.gt(0))
 var mouthCenter = FaceTracking.face(0).mouth.center;
 var endScreen = sceneRoot.child("endScreen")
+var startScreen = sceneRoot.child("startScreen")
 var endScreenScore = endScreen.child("2DCanvas1").child("scoreText")
 var restartButton = endScreen.child("2DCanvas1").child("restartButton")
+var startButton = startScreen.child("2DCanvas1").child("startButton")
 
 var chicken0 = sceneRoot.child("chicken0")
 var chicken1 = sceneRoot.child("chicken1")
@@ -23,12 +26,12 @@ var chicken2 = sceneRoot.child("chicken2")
 var chickens = [{ element: chicken0 }, { element: chicken1 }, { element: chicken2 }]
 
 // lives display
-var lives = sceneRoot.child("lives")
-var life0 = lives.child("life0")
-var life1 = lives.child("life1")
-var life2 = lives.child("life2")
-var life3 = lives.child("life3")
-var life4 = lives.child("life4")
+var livesContainer = sceneRoot.child("lives")
+var life0 = livesContainer.child("life0")
+var life1 = livesContainer.child("life1")
+var life2 = livesContainer.child("life2")
+var life3 = livesContainer.child("life3")
+var life4 = livesContainer.child("life4")
 var livesDisplayElements = [life0, life1, life2, life3, life4]
 
 // global score
@@ -41,10 +44,10 @@ var chickenAnimationTime = 4000
 var maxYdistance = 15
 var maxXdistance = 15
 
-
 var loseLifeBoundary = -15  // below Y line where you lose a life
 var initialLives = 5
 var lives = 0
+var gameState = 'start'
 
 var randomInterval = function (chicken) {
     var randomTime = Math.floor(Math.random() * 3000) + 1000 // 1000 - 4000
@@ -52,7 +55,7 @@ var randomInterval = function (chicken) {
 
     chicken.randomTimeId = Time.setTimeout(function () {
         chicken.element.hidden = false
-        moveChicken(chicken)
+        chicken.mover = moveChicken(chicken)
     }, randomTime)
 }
 
@@ -81,14 +84,16 @@ var moveChicken = function (chickenObject) {
     var sampler = Animation.samplers.linear(27, -40)
     var animationSignal = Animation.animate(driver, sampler)
     chickenObject.animationSignal = animationSignal
+    chickenObject.animationDriver = driver
     chicken.transform.y = animationSignal
 
     driver.start()
 
     driver.onCompleted().subscribe(function (e) {
-        randomInterval(chickenObject)
+        if (gameState === 'playing') randomInterval(chickenObject)
     })
 
+    // monitor chicken off screen for lose life
     chickenObject.animationSignal.lt(loseLifeBoundary).monitor().subscribe(function (e) {
         Diagnostics.log('LOSE LIFE')
         var thisChicken = chicken
@@ -98,8 +103,7 @@ var moveChicken = function (chickenObject) {
 
 var mouthSub = mouthIsOpen.monitor().subscribe(function (e) {
     var mouthX, mouthY
-    if (e.newValue == true) {
-
+    if (e.newValue == true && gameState === 'playing') {
         var mouthX = mouthCenter.x.lastValue
         var mouthY = mouthCenter.y.lastValue
 
@@ -112,7 +116,7 @@ var mouthSub = mouthIsOpen.monitor().subscribe(function (e) {
             if (collision(mouthX, mouthY, chickenX, chickenY, maxXdistance, maxYdistance)) {
                 updateScore(5)
                 Diagnostics.log('EAT!')
-                toggleElement(chickenObject, true)
+                chickenObject.animationDriver.reset()
             }
         })
     } else {
@@ -130,9 +134,13 @@ var collision = function (x1, y1, x2, y2, distanceX, distanceY) {
 
 // start game
 var startGame = function () {
+    gameState = 'playing'
     scoreText.text = "score: " + score.toString()
-    // livesText.text = "lives: " + lives.toString()
     lives += initialLives
+    livesContainer.hidden = false
+    faceTracker.hidden = false
+    scoreTextContainer.hidden = false
+    startScreen.hidden = true
     updateLivesDisplay(lives)
 
     chickens.forEach(function (chicken) {
@@ -146,7 +154,6 @@ var updateLives = function (amount) {
         endGame()
     } else {
         lives += amount
-        // livesText.text = "lives: " + lives.toString()
         updateLivesDisplay(lives)
     }
 }
@@ -159,38 +166,47 @@ var updateLivesDisplay = function (numLives) {
 }
 
 var showEndScreen = function (show) {
+    endScreenScore.text = "Congratulations!\n You Scored:\n " + score.toString()
     endScreen.hidden = !show
-    // TODO: add particles like fireworks to finish?
-    // mini chickens for particles?
-    // simple gif/sprite background
 }
 
-// stop animations
-// stop timers
-// show end score
 var endGame = function () {
+    gameState = 'stopped'
+    livesContainer.hidden = true
+    faceTracker.hidden = true
+    scoreTextContainer.hidden = true
     showEndScreen(true)
-    // TODO: stop timers
 
-    // TODO: stop animations?
+    chickens.forEach(function (chicken) {
+        chicken.animationDriver.reset()
+        chicken.animationDriver.stop()
+        Time.clearTimeout(chicken.randomTimeId)
+    })
 }
 
 // after tap to restart, restart the game
 var restartGame = function () {
-    showEndScore(false)
+    showEndScreen(false)
+    score = 0
     startGame()
 }
 
-// subscribes a tap event for a target
-var tapRegistrar = function (element) {
+var restartTapRegistrar = function (element) {
     TouchGestures.onTap(element).subscribe(function (event) {
         Diagnostics.log('restart!')
         restartGame()
     })
 }
 
-// subscribe to restart button tap
-tapRegistrar(restartButton)
+var startTapRegistrar = function (element) {
+    TouchGestures.onTap(element).subscribe(function (event) {
+        Diagnostics.log('start!')
+        startGame()
+    })
+}
 
+// subscribe to restart button tap
+restartTapRegistrar(restartButton)
+startTapRegistrar(startButton)
 // start the game
-startGame()
+// startGame()
